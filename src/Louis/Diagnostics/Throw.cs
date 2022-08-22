22a5214f1ab6cd28a4c8e0277dec12d02381a362
Riccardo De Agostini <me@riccar.do>
2022-08-22 23:35:28 +0200

    Update text of source file headers. No other changes.

--- src/Louis/Diagnostics/Throw.cs
+++ src/Louis/Diagnostics/Throw.cs
@@ -1,6 +1,5 @@
 ﻿// ---------------------------------------------------------------------------------------
-// Copyright (C) Tenacom and L.o.U.I.S. contributors. All rights reserved.
-// Licensed under the MIT license.
+// Copyright (C) Tenacom and L.o.U.I.S. contributors. Licensed under the MIT license.
 // See the LICENSE file in the project root for full license information.
 //
 // Part of this file may be third-party code, distributed under a compatible license.
