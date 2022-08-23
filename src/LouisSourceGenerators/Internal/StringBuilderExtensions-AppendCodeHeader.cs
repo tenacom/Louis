9a38ef43ec3d293c6f6b1aa7bfa059d8aa3f1417
Riccardo De Agostini <me@riccar.do>
2022-08-24 00:24:13 +0200

    Update source header text in code generators.

--- src/LouisSourceGenerators/Internal/StringBuilderExtensions-AppendCodeHeader.cs
+++ src/LouisSourceGenerators/Internal/StringBuilderExtensions-AppendCodeHeader.cs
@@ -39,8 +39,7 @@ partial class StringBuilderExtensions
 
         _ = @this.AppendLine("""
                              // ---------------------------------------------------------------------------------------
-                             // Copyright (C) Tenacom and L.o.U.I.S. contributors. All rights reserved.
-                             // Licensed under the MIT license.
+                             // Copyright (C) Tenacom and L.o.U.I.S. contributors. Licensed under the MIT license.
                              // See the LICENSE file in the project root for full license information.
                              //
                              // Part of this file may be third-party code, distributed under a compatible license.
