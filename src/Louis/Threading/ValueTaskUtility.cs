bece7220dbb440e76f3195aff8c1052f614ec667
Riccardo De Agostini <me@riccar.do>
2022-11-06 22:34:14 +0100

    Add ValueTaskUtility.WhenAll overload with params keyword

--- src/Louis/Threading/ValueTaskUtility.cs
+++ src/Louis/Threading/ValueTaskUtility.cs
@@ -20,14 +20,31 @@ public static class ValueTaskUtility
     /// <param name="valueTasks">The <see cref="ValueTask"/>s to wait on for completion.</param>
     /// <returns>A <see cref="ValueTask"/> that represents the completion of all of the supplied <see cref="ValueTask"/>s.</returns>
     /// <exception cref="ArgumentNullException"><paramref name="valueTasks"/> is <see langword="null"/>.</exception>
-    public static async ValueTask WhenAll(IEnumerable<ValueTask> valueTasks)
+    public static ValueTask WhenAll(params ValueTask[] valueTasks)
     {
         Guard.IsNotNull(valueTasks);
 
-        var pendingTasks = valueTasks.Where(vt => !vt.IsCompletedSuccessfully).Select(vt => vt.AsTask()).ToList();
-        if (pendingTasks.Count > 0)
-        {
-            await Task.WhenAll(pendingTasks).ConfigureAwait(false);
-        }
+        return UnsafeWhenAll(valueTasks);
+    }
+
+    /// <summary>
+    /// Creates a task that will complete when all of the <see cref="ValueTask"/> objects in an enumerable collection have completed.
+    /// </summary>
+    /// <param name="valueTasks">The <see cref="ValueTask"/>s to wait on for completion.</param>
+    /// <returns>A <see cref="ValueTask"/> that represents the completion of all of the supplied <see cref="ValueTask"/>s.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="valueTasks"/> is <see langword="null"/>.</exception>
+    public static ValueTask WhenAll(IEnumerable<ValueTask> valueTasks)
+    {
+        Guard.IsNotNull(valueTasks);
+
+        return UnsafeWhenAll(valueTasks);
+    }
+
+    private static ValueTask UnsafeWhenAll(IEnumerable<ValueTask> valueTasks)
+    {
+        var pendingTasks = valueTasks.Where(vt => !vt.IsCompletedSuccessfully).Select(vt => vt.AsTask()).ToArray();
+        return pendingTasks.Length > 0
+            ? new ValueTask(Task.WhenAll(pendingTasks))
+            : default;
     }
 }
