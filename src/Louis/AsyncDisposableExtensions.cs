9d47f1de89ccb3ce2d9ab3c17e6b7e5c5cd743c7
Riccardo De Agostini <me@riccar.do>
2022-11-06 21:39:01 +0100

    Add DisposeSynchronously extension for async disposables

---
+++ src/Louis/AsyncDisposableExtensions.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Tenacom and contributors. Licensed under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+
+namespace Louis;
+
+/// <summary>
+/// Provides extension methods for objects implementing <see cref="IAsyncDisposable"/>.
+/// </summary>
+public static class AsyncDisposableExtensions
+{
+    /// <summary>
+    /// Disposes an <see cref="IAsyncDisposable"/> synchronously by awaiting the result of <see cref="IAsyncDisposable.DisposeAsync"/>.
+    /// </summary>
+    /// <param name="this">The <see cref="IAsyncDisposable"/> interface on which this method is called.</param>
+    /// <remarks>
+    /// <para>The most common use case for this method is add an implementation of <see cref="IDisposable.Dispose"/>
+    /// to a type that already implements <see cref="IAsyncDisposable.DisposeAsync"/>, without duplicating any code:</para>
+    /// <code>
+    ///     public void Dispose() => this.DisposeSynchronously();
+    /// </code>
+    /// </remarks>
+    public static void DisposeSynchronously(this IAsyncDisposable @this)
+#pragma warning disable CA2012 // Use ValueTasks correctly - ValueTask gets consumed only once, hence this is correct usage.
+        => @this.DisposeAsync().GetAwaiter().GetResult();
+#pragma warning restore CA2012 // Use ValueTasks correctly
+}
