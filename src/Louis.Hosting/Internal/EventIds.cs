242846077e8c07ada620b2f16e67ddf96e135a1d
Tenacom developer <info@tenacom.it>
2023-07-07 00:38:33 +0200

    ADD: AsyncHostedService, a subclass of AsyncService that implements IHostedService

---
+++ src/Louis.Hosting/Internal/EventIds.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Tenacom and contributors. Licensed under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+namespace Louis.Hosting.Internal;
+
+internal static class EventIds
+{
+    public static class AsyncHostedService
+    {
+        public const int StateChanged = 0;
+        public const int SetupCanceled = 1;
+        public const int SetupFailed = 2;
+        public const int ExecuteCanceled = 3;
+        public const int ExecuteFailed = 4;
+        public const int TeardownFailed = 5;
+    }
+}
