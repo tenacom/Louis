c2eea1b617ee6228543b13f7b5abe92f4ddfad28
Tenacom developer <info@tenacom.it>
2023-07-23 03:09:47 +0200

    Renumber event IDs starting from 1 instead of 0
    Event ID 0 is commonly considered as "no event ID present".

--- src/Louis.Hosting/Internal/EventIds.cs
+++ src/Louis.Hosting/Internal/EventIds.cs
@@ -7,20 +7,20 @@ internal static class EventIds
 {
     public static class AsyncHostedService
     {
-        public const int StateChanged = 0;
-        public const int BeforeSetup = 1;
-        public const int SetupCompleted = 2;
-        public const int SetupCanceled = 3;
-        public const int SetupFailed = 4;
-        public const int BeforeExecute = 5;
-        public const int ExecuteCompleted = 6;
-        public const int ExecuteCanceled = 7;
-        public const int ExecuteFailed = 8;
-        public const int BeforeTeardown = 9;
-        public const int TeardownCompleted = 10;
-        public const int TeardownFailed = 11;
-        public const int StopRequested = 12;
-        public const int HostedServiceStarting = 13;
-        public const int HostedServiceStopping = 14;
+        public const int StateChanged = 1;
+        public const int BeforeSetup = 2;
+        public const int SetupCompleted = 3;
+        public const int SetupCanceled = 4;
+        public const int SetupFailed = 5;
+        public const int BeforeExecute = 6;
+        public const int ExecuteCompleted = 7;
+        public const int ExecuteCanceled = 8;
+        public const int ExecuteFailed = 9;
+        public const int BeforeTeardown = 10;
+        public const int TeardownCompleted = 11;
+        public const int TeardownFailed = 12;
+        public const int StopRequested = 13;
+        public const int HostedServiceStarting = 14;
+        public const int HostedServiceStopping = 15;
     }
 }
