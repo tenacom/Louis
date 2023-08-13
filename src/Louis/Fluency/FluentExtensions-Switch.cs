d05ebdd7b833c97f634a06c46d8e288ae947f59d
Tenacom developer <info@tenacom.it>
2023-08-13 06:22:10 +0200

    Refactor FluentExtensions.Switch to use equality comparers

--- src/Louis/Fluency/FluentExtensions-Switch.cs
+++ src/Louis/Fluency/FluentExtensions-Switch.cs
@@ -2,6 +2,7 @@
 // See the LICENSE file in the project root for full license information.
 
 using System;
+using System.Collections.Generic;
 using CommunityToolkit.Diagnostics;
 
 namespace Louis.Fluency;
@@ -31,8 +32,33 @@ partial class FluentExtensions
     /// <para>If <paramref name="value"/> is not equal to any of the values in <paramref name="cases"/>, this method returns immediately.</para>
     /// </remarks>
     public static T Switch<T, TValue>(this T @this, TValue value, params (TValue Comparand, FluentAction<T>? Action)[] cases)
-        where TValue : IEquatable<TValue>
-        => Switch(@this, value, null, cases);
+        => Switch(@this, EqualityComparer<TValue>.Default, value, null, cases);
+
+    /// <summary>
+    /// Selects an action to invoke on an object by comparing a given value against a list of comparands, then returns the object.
+    /// </summary>
+    /// <typeparam name="T">The type of the object.</typeparam>
+    /// <typeparam name="TValue">The type of the value being compared.</typeparam>
+    /// <param name="this">The object on which this method was called.</param>
+    /// <param name="comparer">The interface to use for comparisons.</param>
+    /// <param name="value">The value to compare.</param>
+    /// <param name="cases">
+    /// <para>An array of pairs, where each element consists of a value to compare against <paramref name="value"/>
+    /// and an optional action to perform on <paramref name="this"/> if they are equal.</para>
+    /// <para>If the action is <see langword="null"/>, no action is performed if the value is equal to <paramref name="value"/>;
+    /// instead, the method returns immediately.</para>
+    /// </param>
+    /// <returns>A reference to <paramref name="this"/> after one of the actions in <paramref name="cases"/>, if any, returns.</returns>
+    /// <exception cref="ArgumentNullException">
+    /// <para><paramref name="this"/> is <see langword="null"/>.</para>
+    /// <para>- or -</para>
+    /// <para><paramref name="cases"/> is <see langword="null"/>.</para>
+    /// </exception>
+    /// <remarks>
+    /// <para>If <paramref name="value"/> is not equal to any of the values in <paramref name="cases"/>, this method returns immediately.</para>
+    /// </remarks>
+    public static T Switch<T, TValue>(this T @this, IEqualityComparer<TValue> comparer, TValue value, params (TValue Comparand, FluentAction<T>? Action)[] cases)
+        => Switch(@this, comparer, value, null, cases);
 
     /// <summary>
     /// Selects an action to invoke on an object by comparing a given value against a list of comparands, then returns the object.
@@ -57,8 +83,33 @@ partial class FluentExtensions
     /// <para>If <paramref name="value"/> is not equal to any of the values in <paramref name="cases"/>, this method returns immediately.</para>
     /// </remarks>
     public static T Switch<T, TValue>(this T @this, TValue value, params (TValue Comparand, Action<T>? Action)[] cases)
-        where TValue : IEquatable<TValue>
-        => Switch(@this, value, null, cases);
+        => Switch(@this, EqualityComparer<TValue>.Default, value, null, cases);
+
+    /// <summary>
+    /// Selects an action to invoke on an object by comparing a given value against a list of comparands, then returns the object.
+    /// </summary>
+    /// <typeparam name="T">The type of the object.</typeparam>
+    /// <typeparam name="TValue">The type of the value being compared.</typeparam>
+    /// <param name="this">The object on which this method was called.</param>
+    /// <param name="comparer">The interface to use for comparisons.</param>
+    /// <param name="value">The value to compare.</param>
+    /// <param name="cases">
+    /// <para>An array of pairs, where each element consists of a value to compare against <paramref name="value"/>
+    /// and an optional action to perform on <paramref name="this"/> if they are equal.</para>
+    /// <para>If the action is <see langword="null"/>, no action is performed if the value is equal to <paramref name="value"/>;
+    /// instead, the method returns immediately.</para>
+    /// </param>
+    /// <returns>A reference to <paramref name="this"/> after one of the actions in <paramref name="cases"/>, if any, returns.</returns>
+    /// <exception cref="ArgumentNullException">
+    /// <para><paramref name="this"/> is <see langword="null"/>.</para>
+    /// <para>- or -</para>
+    /// <para><paramref name="cases"/> is <see langword="null"/>.</para>
+    /// </exception>
+    /// <remarks>
+    /// <para>If <paramref name="value"/> is not equal to any of the values in <paramref name="cases"/>, this method returns immediately.</para>
+    /// </remarks>
+    public static T Switch<T, TValue>(this T @this, IEqualityComparer<TValue> comparer, TValue value, params (TValue Comparand, Action<T>? Action)[] cases)
+        => Switch(@this, comparer, value, null, cases);
 
     /// <summary>
     /// Selects an action to invoke on an object by comparing a given value against a list of comparands, then returns the object.
@@ -86,14 +137,43 @@ partial class FluentExtensions
     /// <para><paramref name="cases"/> is <see langword="null"/>.</para>
     /// </exception>
     public static T Switch<T, TValue>(this T @this, TValue value, FluentAction<T>? @default, params (TValue Comparand, FluentAction<T>? Action)[] cases)
-        where TValue : IEquatable<TValue>
+        => Switch(@this, EqualityComparer<TValue>.Default, value, @default, cases);
+
+    /// <summary>
+    /// Selects an action to invoke on an object by comparing a given value against a list of comparands, then returns the object.
+    /// </summary>
+    /// <typeparam name="T">The type of the object.</typeparam>
+    /// <typeparam name="TValue">The type of the value being compared.</typeparam>
+    /// <param name="this">The object on which this method was called.</param>
+    /// <param name="comparer">The interface to use for comparisons.</param>
+    /// <param name="value">The value to compare.</param>
+    /// <param name="default">
+    /// <para>An optional action to perform on <paramref name="this"/> if <paramref name="value"/> is not equal
+    /// to any of the values in <paramref name="cases"/>.</para>
+    /// <para>If this parameter is <see langword="null"/> and <paramref name="value"/> is not equal
+    /// to any of the values in <paramref name="cases"/>, this method returns immediately.</para>
+    /// </param>
+    /// <param name="cases">
+    /// <para>An array of pairs, where each element consists of a value to compare against <paramref name="value"/>
+    /// and an optional action to perform on <paramref name="this"/> if they are equal.</para>
+    /// <para>If the action is <see langword="null"/>, no action is performed if the value is equal to <paramref name="value"/>;
+    /// instead, the method returns immediately.</para>
+    /// </param>
+    /// <returns>A reference to <paramref name="this"/> after one of the actions in <paramref name="cases"/>, if any, returns.</returns>
+    /// <exception cref="ArgumentNullException">
+    /// <para><paramref name="this"/> is <see langword="null"/>.</para>
+    /// <para>- or -</para>
+    /// <para><paramref name="cases"/> is <see langword="null"/>.</para>
+    /// </exception>
+    public static T Switch<T, TValue>(this T @this, IEqualityComparer<TValue> comparer, TValue value, FluentAction<T>? @default, params (TValue Comparand, FluentAction<T>? Action)[] cases)
     {
         Guard.IsNotNull(@this);
+        Guard.IsNotNull(comparer);
         Guard.IsNotNull(cases);
 
         foreach (var (comparand, action) in cases)
         {
-            if (value.Equals(comparand))
+            if (comparer.Equals(value, comparand))
             {
                 return action == null ? @this : action(@this);
             }
@@ -128,14 +208,43 @@ partial class FluentExtensions
     /// <para><paramref name="cases"/> is <see langword="null"/>.</para>
     /// </exception>
     public static T Switch<T, TValue>(this T @this, TValue value, FluentAction<T>? @default, params (TValue Comparand, Action<T>? Action)[] cases)
-        where TValue : IEquatable<TValue>
+        => Switch(@this, EqualityComparer<TValue>.Default, value, @default, cases);
+
+    /// <summary>
+    /// Selects an action to invoke on an object by comparing a given value against a list of comparands, then returns the object.
+    /// </summary>
+    /// <typeparam name="T">The type of the object.</typeparam>
+    /// <typeparam name="TValue">The type of the value being compared.</typeparam>
+    /// <param name="this">The object on which this method was called.</param>
+    /// <param name="comparer">The interface to use for comparisons.</param>
+    /// <param name="value">The value to compare.</param>
+    /// <param name="default">
+    /// <para>An optional action to perform on <paramref name="this"/> if <paramref name="value"/> is not equal
+    /// to any of the values in <paramref name="cases"/>.</para>
+    /// <para>If this parameter is <see langword="null"/> and <paramref name="value"/> is not equal
+    /// to any of the values in <paramref name="cases"/>, this method returns immediately.</para>
+    /// </param>
+    /// <param name="cases">
+    /// <para>An array of pairs, where each element consists of a value to compare against <paramref name="value"/>
+    /// and an optional action to perform on <paramref name="this"/> if they are equal.</para>
+    /// <para>If the action is <see langword="null"/>, no action is performed if the value is equal to <paramref name="value"/>;
+    /// instead, the method returns immediately.</para>
+    /// </param>
+    /// <returns>A reference to <paramref name="this"/> after one of the actions in <paramref name="cases"/>, if any, returns.</returns>
+    /// <exception cref="ArgumentNullException">
+    /// <para><paramref name="this"/> is <see langword="null"/>.</para>
+    /// <para>- or -</para>
+    /// <para><paramref name="cases"/> is <see langword="null"/>.</para>
+    /// </exception>
+    public static T Switch<T, TValue>(this T @this, IEqualityComparer<TValue> comparer, TValue value, FluentAction<T>? @default, params (TValue Comparand, Action<T>? Action)[] cases)
     {
         Guard.IsNotNull(@this);
+        Guard.IsNotNull(comparer);
         Guard.IsNotNull(cases);
 
         foreach (var (comparand, action) in cases)
         {
-            if (value.Equals(comparand))
+            if (comparer.Equals(value, comparand))
             {
                 action?.Invoke(@this);
                 return @this;
