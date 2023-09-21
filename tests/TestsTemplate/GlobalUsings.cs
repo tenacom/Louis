92dc9a4cb7a77f0e620f701f84f3fda9b8f24a7a
Tenacom developer <info@tenacom.it>
2023-09-21 23:47:22 +0200

    Add a template for test projects

---
+++ tests/TestsTemplate/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿// Copyright (c) Tenacom and contributors. Licensed under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+global using System;
+global using FluentAssertions;
+global using Xunit;
