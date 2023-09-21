464ac643a7b7409f612593c8ef9404cf3d8f554b
Tenacom developer <info@tenacom.it>
2023-09-21 23:46:57 +0200

    Remove logging tests

--- tests/LoggingTests/GlobalUsings.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Tenacom and contributors. Licensed under the MIT license.
-// See the LICENSE file in the project root for full license information.
-
-global using System;
-global using FluentAssertions;
-global using Internal;
-global using Louis.Logging;
-global using Microsoft.Extensions.Logging;
-global using Xunit;
